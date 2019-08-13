da0378909aa92e4bb0ab6f5be596137a4338ce5d
liangqing <245374011@qq.com>
2019-08-13 21:12:46 +0800

    驾照测试完毕

--- renren-security/renren-api/src/main/java/io/renren/controller/TestController.java
+++ renren-security/renren-api/src/main/java/io/renren/controller/TestController.java
@@ -1,5 +1,6 @@
 package io.renren.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.mysql.jdbc.StringUtils;
 import io.renren.ApiApplication;
@@ -8,14 +9,14 @@ import io.renren.entity.Driver;
 import io.renren.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 功能描述: <br>
@@ -28,7 +29,8 @@ import java.util.List;
 @RequestMapping("/test")
 public class TestController {
     private static Logger logger = LoggerFactory.getLogger(TestController.class);
-
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
     @Resource
     private UserService userService;
 
@@ -36,6 +38,7 @@ public class TestController {
     @RequestMapping("/get")
     public R get() throws IOException {
         List<Driver> driverList = userService.queryDrver();
+
         for (Driver driver:driverList){
             String driverName = driver.getCountry(); // Canada
             String driverPreName = driver.getRemark(); // CA 国家缩写
@@ -46,55 +49,60 @@ public class TestController {
             String driverFileName = "D:" + driver.getTempPic(); // 原始驾照模板图片路径 D:/upload/201904/27/201904271751127196.jpg
             logger.info("原始驾照图片地址：" + driverFileName);
 
-            int temp = 0;
-            String driver_suffix = driverFileName.substring(driverFileName.lastIndexOf("."));
-            StringBuilder driverNewFileName = new StringBuilder(driverPreName); //新驾照模板文件名 CA_0.jpg
-            if(!StringUtils.isNullOrEmpty(driverPro)){
-                driverNewFileName.append("_").append(driverPro);
-            }
-            logger.info("新驾照名称路径：" + driverPath + "/" + driverNewFileName  + "_" + temp + driver_suffix);
-            while (judeFileExists(new File(driverPath + "/" + driverNewFileName + "_" + temp + driver_suffix))){ // 如果文件存在 就加一
-                logger.info("该路径已存在，数量+1");
-                temp++;
-            }
+            if(!redisTemplate.opsForSet().isMember("o_driver",driver.getTempPic())){ //该驾照是新的
+                redisTemplate.opsForSet().add("o_driver",driver.getTempPic());
 
-            driverNewFileName.append("_").append(temp).append(driver_suffix);
-            String newFilePath = driverPath + "/" + driverNewFileName;
-            logger.info("最终驾照新文件：" + newFilePath);
-            if(!isExist(new File(newFilePath))){
+                int temp = 0;
+                String driver_suffix = driverFileName.substring(driverFileName.lastIndexOf("."));
+                StringBuilder driverNewFileName = new StringBuilder(driverPreName); //新驾照模板文件名 CA
+                if(!StringUtils.isNullOrEmpty(driverPro)){
+                    driverNewFileName.append("_").append(driverPro);
+                }
+                logger.info("新驾照名称路径：" + driverPath + "/" + driverNewFileName  + "_" + temp + driver_suffix);
+                while (isExist(new File(driverPath + "/" + driverNewFileName + "_" + temp + driver_suffix))){ // 如果文件存在 就加一
+                    logger.info("该路径已存在，数量+1," + temp);
+                    if(temp == 20){
+                        break;
+                    }
+                    temp++;
+                }
+
+                driverNewFileName.append("_").append(temp).append(driver_suffix);
+                String newFilePath = driverPath + "/" + driverNewFileName;
+                logger.info("最终驾照新文件：" + newFilePath);
                 saveFile(driverFileName,newFilePath);
             }else{
                 logger.info("驾照文件已存在，跳过");
             }
 
+
             if(!StringUtils.isNullOrEmpty(driver.getForgeryPic())){
-                String forgeryPicName = "D:" + driver.getForgeryPic(); //防伪图原路径
-                logger.info("原始驾照防伪图片地址：" + forgeryPicName);
+                if(!redisTemplate.opsForSet().isMember("o_driver_m",driver.getForgeryPic())) { //该驾照防伪是新的
+                    redisTemplate.opsForSet().add("o_driver_m",driver.getForgeryPic());
 
-                int temp1 = 0;
-                String forgery_suffix = forgeryPicName.substring(forgeryPicName.lastIndexOf("."));
-                StringBuilder forgeryNewFileName = new StringBuilder(forgery_suffix); //新驾照防伪图片文件名 CA_m_0.jpg
-                if(!StringUtils.isNullOrEmpty(driverPro)){
-                    forgeryNewFileName.append("_").append(driverPro);
-                }
-                logger.info("新驾照防伪名称路径：" +driverPath + "/" +  driverNewFileName  + "_m_" + temp1 + driver_suffix);
-                while (judeFileExists(new File(driverPath + "/" + driverNewFileName + "_m_" + temp1 + driver_suffix))){ // 如果文件存在 就加一
-                    logger.info("该路径已存在，数量+1");
-                    temp1++;
-                }
+                    String forgeryPicName = "D:" + driver.getForgeryPic(); //防伪图原路径
+                    logger.info("原始驾照防伪图片地址：" + forgeryPicName);
 
-                forgeryNewFileName.append("_").append(temp1).append(forgery_suffix);
-                String newForgeryFilePath = driverPath + "/" + forgeryNewFileName;
-                logger.info("最终驾照防伪新文件：" + newForgeryFilePath);
-                if(!isExist(new File(newFilePath))){
+                    int temp1 = 0;
+                    String forgery_suffix = forgeryPicName.substring(forgeryPicName.lastIndexOf("."));
+                    StringBuilder forgeryNewFileName = new StringBuilder(driverPreName); //新驾照防伪图片文件名 CA
+                    if(!StringUtils.isNullOrEmpty(driverPro)){
+                        forgeryNewFileName.append("_").append(driverPro);
+                    }
+                    logger.info("新驾照防伪名称路径：" + driverPath + "/" +  forgeryNewFileName  + "_m_" + temp1 + forgery_suffix);
+                    while (isExist(new File(driverPath + "/" + forgeryNewFileName + "_m_" + temp1 + forgery_suffix))){ // 如果文件存在 就加一
+                        logger.info("该路径已存在，数量+1");
+                        temp1++;
+                    }
+
+                    forgeryNewFileName.append("_m_").append(temp1).append(forgery_suffix);
+                    String newForgeryFilePath = driverPath + "/" + forgeryNewFileName;
+                    logger.info("最终驾照防伪新文件：" + newForgeryFilePath);
                     saveFile(forgeryPicName,newForgeryFilePath);
                 }else{
                     logger.info("驾照防伪文件已存在，跳过");
                 }
-
             }
-
-            break;
         }
         return R.ok();
     }
